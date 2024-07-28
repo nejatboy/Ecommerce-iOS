5aa74e6dc9236609043b8bf90de42d9e31984781
Nejat Boy <nejatboy34@gmail.com>
2024-07-28 23:05:53 +0300

    Base development.

---
+++ Ecommerce/base/Views.swift
@@ -0,0 +1,61 @@
+//
+//  Views.swift
+//  Ecommerce
+//
+//  Created by Nejat Boy on 28.07.2024.
+//
+
+import UIKit
+
+
+// MARK: Label
+class Label: UILabel {
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+
+// MARK: Button
+class Button: UIButton {
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+
+// MARK: TextField
+class TextField: UITextField {
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
