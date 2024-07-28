0cc41cab4c17b945ce9c95316dceab6b64419fe4
Nejat Boy <nejatboy34@gmail.com>
2024-07-28 21:38:00 +0300

    Update SceneDelegate.swift

--- Ecommerce/SceneDelegate.swift
+++ Ecommerce/SceneDelegate.swift
@@ -13,13 +13,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
         guard let scene = (scene as? UIWindowScene) else{
             return
         }
+        
         let window = UIWindow(windowScene: scene)
         window.rootViewController = ViewController()
         window.makeKeyAndVisible()
+        
         self.window = window
     }
 
