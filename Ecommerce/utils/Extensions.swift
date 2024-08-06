a23dc04c64967aa0478111a3c2c40d40ff84a3df
Nejat Boy <nejatboy34@gmail.com>
2024-08-07 01:06:34 +0300

    AuthService development.

---
+++ Ecommerce/utils/Extensions.swift
@@ -0,0 +1,11 @@
+//
+//  Extensions.swift
+//  Ecommerce
+//
+//  Created by Nejat Boy on 7.08.2024.
+//
+
+
+//MARK: TypeAlias
+typealias Handler = (() -> Void)
+typealias Callback<T> = (T) -> Void
