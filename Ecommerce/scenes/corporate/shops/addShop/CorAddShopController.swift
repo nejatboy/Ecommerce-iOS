b2f6680930b45eb5f9cca389cfeffa7988292c3c
muhammed dursun <muhammedrsn35@gmail.com>
2024-09-15 18:53:01 +0300

    Fixed add_shop branch.

--- Ecommerce/scenes/corporate/shops/addShop/CorAddShopController.swift
+++ Ecommerce/scenes/corporate/shops/addShop/CorAddShopController.swift
@@ -10,100 +10,99 @@ import MapKit
 
 class CorAddShopController: Controller<CorAddShopViewModel, CorShopsNavigationController>, CLLocationManagerDelegate, MKMapViewDelegate {
     
-    private let nameTextField = TextFieldLayout()
-    private let addShopButton = ButtonSecondary()
-    private let mapView = MKMapView()
-    private let locationManager = CLLocationManager()
-    private var selectedCoordinate: CLLocationCoordinate2D?
     
+     private let nameTextField = TextFieldLayout()
+     private let addShopButton = ButtonSecondary()
+     private let mapView = MKMapView()
+     private let locationManager = CLLocationManager()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        navigationItem.title = "Add Shop"
-        mapView.delegate = self
-        
-        addSubviews(nameTextField,addShopButton,mapView)
-        
-        activateConstraints(
-            mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 20),
-            mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            mapView.heightAnchor.constraint(equalToConstant: 300),
-            mapView.widthAnchor.constraint(equalToConstant: Device.width * 0.8),
-            
-            nameTextField.topAnchor.constraint(equalTo: mapView.bottomAnchor, constant: 20),
-            nameTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            
-            addShopButton.topAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: 20),
-            addShopButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        )
-        
-        setUpMapView()
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(mapViewTapped(_:)))
-        mapView.addGestureRecognizer(tapGesture)
-    }
-    
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        
-        mapView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 300)
-    }
-    
-    
-    private func setUpMapView() {
-        locationManager.delegate = self
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
-        mapView.showsUserLocation = true
-    }
-    
-    
-    @objc private func mapViewTapped(_ gesture: UITapGestureRecognizer) {
-        let location = gesture.location(in: mapView)
-        let coordinate = mapView.convert(location, toCoordinateFrom: mapView)
-        
-        if let previousAnnotation = mapView.annotations.first {
-            mapView.removeAnnotation(previousAnnotation)
-        }
-        
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = coordinate
-        annotation.title = "Selected Location"
-        mapView.addAnnotation(annotation)
-        
-        selectedCoordinate = coordinate
+     
+     
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         
+         navigationItem.title = "Add Shop"
+         mapView.delegate = self
+         
+         addSubviews(nameTextField, addShopButton, mapView)
+         
+         mapView.translatesAutoresizingMaskIntoConstraints = false
+         
+         activateConstraints(
+             mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+             mapView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,constant: 10),
+             mapView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+             mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+             mapView.heightAnchor.constraint(equalToConstant: 300),
+             
+             nameTextField.topAnchor.constraint(equalTo: mapView.bottomAnchor, constant: 20),
+             nameTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+             
+             addShopButton.topAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: 20),
+             addShopButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+         )
+         
+         setUpMapView()
+         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(mapViewTapped(_:)))
+         mapView.addGestureRecognizer(tapGesture)
+     }
+     
+   
+     private func setUpMapView() {
+         locationManager.delegate = self
+         locationManager.requestWhenInUseAuthorization()
+         locationManager.startUpdatingLocation()
+         mapView.showsUserLocation = true
+     }
+     
+     
+     @objc private func mapViewTapped(_ gesture: UITapGestureRecognizer) {
+         let location = gesture.location(in: mapView)
+         let coordinate = mapView.convert(location, toCoordinateFrom: mapView)
+         
+         if let previousAnnotation = mapView.annotations.first {
+             mapView.removeAnnotation(previousAnnotation)
+         }
+         
+         let annotation = MKPointAnnotation()
+         annotation.coordinate = coordinate
+         annotation.title = "Selected Location"
+         mapView.addAnnotation(annotation)
+         
+         viewModel.updateCoordinate(for: CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude))
+     }
+     
+     
+     override func customizeViews() {
+         nameTextField.placeholder = "Name"
+         
+         addShopButton.setTitle("Add Shop", for: .normal)
+         addShopButton.action = addShopButtonClicked
+     }
+     
+     
+     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+           guard let location = locations.last else { return }
+         viewModel.updateCoordinate(for: location)
+         
+         if let region = viewModel.createRegion() {
+             mapView.setRegion(region, animated: true)
+         }
        }
-    
-    
-    override func customizeViews() {
-        nameTextField.placeholder = "Name"
-        
-        addShopButton.setTitle("Add Shop", for: .normal)
-        addShopButton.action = addShopButtonClicked
-    }
-    
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-          guard let location = locations.last else { return }
-          let coordinate = location.coordinate
-          let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
-          mapView.setRegion(region, animated: true)
-      }
-    
-        
-    private func addShopButtonClicked() {
-        guard
-            let name = nameTextField.text,
-            let shopLocation = selectedCoordinate
-        else {
-            return
-        }
-        
-        showLoading()
-        
-        viewModel.addShop(name: name, latitude: shopLocation.latitude, longitude: shopLocation.longitude) {
-            self.show(message: "Shop added succesfully", type: .success)
-        }
-    }
+     
+         
+     private func addShopButtonClicked() {
+         guard
+             let name = nameTextField.text,
+             let shopLocation = viewModel.selectedCoordinate
+         else {
+             return
+         }
+         
+         showLoading()
+         
+         viewModel.addShop(name: name, latitude: shopLocation.latitude, longitude: shopLocation.longitude) {
+             self.show(message: "Shop added succesfully", type: .success)
+         }
+     }
 }
--- Ecommerce/scenes/corporate/shops/addShop/CorAddShopViewModel.swift
+++ Ecommerce/scenes/corporate/shops/addShop/CorAddShopViewModel.swift
@@ -5,11 +5,26 @@
 //  Created by Nejat Boy on 8.09.2024.
 //
 
+import MapKit
+
 
 class CorAddShopViewModel: ViewModel {
     
     
+    private(set) var selectedCoordinate : CLLocationCoordinate2D?
+    
+    
     func addShop(name: String,latitude: Double, longitude: Double,completion: Handler?) {
         DatabaseService.instance.addShop(shop: Shop(name: name, latitude: latitude, longitude: longitude), completion: completion)
+    }
+    
+    
+    func updateCoordinate(for location: CLLocation) {
+        selectedCoordinate = location.coordinate
+    }
+    
+    func createRegion() -> MKCoordinateRegion? {
+        guard let coordinate = selectedCoordinate else { return nil }
+        return MKCoordinateRegion(center: coordinate, latitudinalMeters: 1000,longitudinalMeters: 1000)
     }
 }
