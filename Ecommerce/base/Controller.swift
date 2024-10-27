85a21de4a993028ef61164fd70b1d58eab366965
Nejat Boy <nejatboy34@gmail.com>
2024-10-27 19:14:55 +0300

    Deletion hideLoading, showLoading, showMessage from Controller.

--- Ecommerce/base/Controller.swift
+++ Ecommerce/base/Controller.swift
@@ -53,44 +53,10 @@ class Controller<VM: ViewModel, NC: NavigationController>: UIViewController {
     }
     
     
-    /// Loading göstermek için kullanırız.
-    func showLoading() {
-        LoadingView.instance.show()
-    }
-    
-    
-    /// Loading gizlemek için kullanırız.
-    func hideLoading() {
-        LoadingView.instance.hide()
-    }
-    
-    
-    /// Kullanıcıya mesaj göstermek için kullanılırız.
-    ///
-    /// - Parameters:
-    ///   - message: Gösterilecek olan mesaj.
-    ///   - type: Başarılı / Hata / Uyarı mesaj tipleridir.
-    func show(message: String?, type: AlertType) {
-        let okAction = AlertModel(title: "Okay")
-        AlertView.instance.show(type: type, message: message, actions: [okAction])
-    }
-    
-    
     ///Navigation Bar'ın sağına button eklemek için kullanırız.
     func setNavButton(icon: UIImage?, onClicked: Handler?) {
         navigationBarClicked = onClicked
         navigationItem.rightBarButtonItem = .init(image: icon, style: .done, target: self, action: #selector(navButtonClicked))
-    }
-    
-    
-    /// Kullanıcıya alert göstermek için kullanılırız.
-    ///
-    /// - Parameters:
-    ///   - message: Gösterilecek olan mesaj.
-    ///   - type: Başarılı / Hata / Uyarı mesaj tipleridir.
-    ///   - actions: Tıklanma işlemleri
-    func showAlert(type: AlertType, message: String?, actions: [AlertModel]) {
-        AlertView.instance.show(type: type, message: message, actions: actions)
     }
     
     
