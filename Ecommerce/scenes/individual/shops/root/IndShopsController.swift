c842d9f31c7712b1d2b5bfabb9186f0b64674d9c
muhammed dursun <muhammedrsn35@gmail.com>
2024-10-27 08:55:02 +0300

    Feature/individual_show_shops branch development.

--- Ecommerce/scenes/individual/shops/root/IndShopsController.swift
+++ Ecommerce/scenes/individual/shops/root/IndShopsController.swift
@@ -8,9 +8,52 @@
 class IndShopsController: Controller<IndShopsViewModel, IndShopsNavigationController> {
     
     
+    private let tableView = IndShopsTableView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "Shops"
+        
+        addSubviews(tableView)
+        
+        activateConstraints(
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        )
+        
+        viewModel.fetchLocation { coordinate in
+            self.viewModel.fetchShops(coordinate: coordinate) { shops in
+                if let shops = shops {
+                    self.tableView.addItems(shops)
+                }
+                else {
+                    let okAction = AlertModel(title: "OK")
+                    self.showAlert(type: .error, message: "Dükkan bulunamadı.", actions: [okAction] )
+                }
+                
+            }
+        }
     }
+    
+  /*  override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        viewModel.fetchLocation { coordinate in
+            self.viewModel.fetchShops(coordinate: coordinate) { shops in
+                if let shops = shops {
+                    self.tableView.addItems(shops)
+                }
+                else {
+                    let okAction = AlertModel(title: "OK")
+                    self.showAlert(type: .error, message: "Dükkan bulunamadı.", actions: [okAction] )
+                }
+                
+            }
+        }
+    } */
+    
 }
--- Ecommerce/scenes/individual/shops/root/IndShopsViewModel.swift
+++ Ecommerce/scenes/individual/shops/root/IndShopsViewModel.swift
@@ -5,6 +5,32 @@
 //  Created by Nejat Boy on 26.10.2024.
 //
 
+import CoreLocation
+
+
 class IndShopsViewModel: ViewModel {
     
+    /// MARK : KONUM OLARAK XCODE'DA Feature bölümünden simülatörün konumu Ankaranın enlem ve boylam değerleri girildi.
+    func fetchShops(coordinate: Coordinate, completion: Callback<[Shop]?>?) {
+        DatabaseService.instance.getShops(coordinate: coordinate) { shops in
+            guard let shops = shops else {
+                return
+            }
+          
+            let userLocation = CLLocation(latitude: coordinate.latitude!, longitude: coordinate.longitude!)
+            
+            let maxDistance: CLLocationDistance = 900 * 100
+            
+            let nearbyShops = shops.filter { shop in
+                let shopLocation = CLLocation(latitude: shop.latitude!, longitude: shop.longitude!)
+                return userLocation.distance(from: shopLocation) <= maxDistance
+            }
+            completion?(nearbyShops)
+        }
+    }
+    
+    
+    func fetchLocation(listener: Callback<Coordinate>?) {
+        LocationService.instance.request(listener: listener)
+    }
 }
