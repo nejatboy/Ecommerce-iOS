af0e840ee4ef3553d69389062ba91db1d4194cf6
Nejat Boy <nejatboy34@gmail.com>
2024-12-08 04:10:06 +0300

    IndProductDetail created.

---
+++ Ecommerce/scenes/individual/shops/productDetail/IndProductDetailController.swift
@@ -0,0 +1,16 @@
+//
+//  IndProductDetailController.swift
+//  Ecommerce
+//
+//  Created by Nejat Boy on 8.12.2024.
+//
+
+class IndProductDetail: Controller<IndProductDetailViewModel, IndShopsNavigationController> {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
