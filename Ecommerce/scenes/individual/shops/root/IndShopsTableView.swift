a1b55c405032b41ba9ac911e66b529534c8cb456
muhammed dursun <muhammedrsn35@gmail.com>
2024-12-12 23:07:44 +0300

    refactoring/add_shop branch development.

--- Ecommerce/scenes/individual/shops/root/IndShopsTableView.swift
+++ Ecommerce/scenes/individual/shops/root/IndShopsTableView.swift
@@ -14,28 +14,48 @@ class IndShopsTableView: TableView<Shop, IndShopsCell> {
 
 
 class IndShopsCell: TableViewCell<Shop> {
-    
+   
+    private let shopsImageView = ImageView()
     private let shopsName = Label()
+    private let distanceShopsLabel = Label()
     
     
     override func configure() {
         selectionBackgroundColor = .lightGray
         backgroundColor = .clear
         
-        shopsName.textColor = .black
+        shopsName.textColor =  .black.withAlphaComponent(1.0)
+        shopsName.font = .setDynamicFont(size: 16)
         
-        addSubviews(shopsName)
+        distanceShopsLabel.textColor = .lightGray
+        distanceShopsLabel.font = .setDynamicFont(size: 16)
+        
+        shopsImageView.contentMode = .scaleAspectFill
+        shopsImageView.clipsToBounds = true
+        shopsImageView.layer.cornerRadius = 10
+        
+        addSubviews(shopsImageView, shopsName, distanceShopsLabel)
         
         activateConstraints(
-            shopsName.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 3),
-            shopsName.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 3),
+            shopsImageView.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 6),
+            shopsImageView.leadingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leadingAnchor, constant: 6),
+            shopsImageView.widthAnchor.constraint(equalToConstant: 80),
+            shopsImageView.heightAnchor.constraint(equalToConstant: 80),
             
-            contentView.bottomAnchor.constraint(equalTo: shopsName.bottomAnchor, constant: 10)
+            shopsName.topAnchor.constraint(equalTo: shopsImageView.topAnchor, constant: 4),
+            shopsName.leadingAnchor.constraint(equalTo: shopsImageView.trailingAnchor, constant: 6),
+            
+            distanceShopsLabel.topAnchor.constraint(equalTo: shopsName.bottomAnchor, constant: 6),
+            distanceShopsLabel.leadingAnchor.constraint(equalTo: shopsName.leadingAnchor, constant: 0),
+            
+            contentView.bottomAnchor.constraint(equalTo: shopsImageView.bottomAnchor, constant: 10)
         )
     }
     
     
     override func setItem(_ item: Shop) {
         shopsName.text = item.name
+        shopsImageView.load(photoUrl: item.imageUrl)
+        distanceShopsLabel.text = "5 km"
     }
 }
